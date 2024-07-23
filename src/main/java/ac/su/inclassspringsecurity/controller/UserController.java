0a499cb0305e89951b7145d352a05356ffc73aaf
Ir0nWaterX2 <108861088+Ir0nWaterX2@users.noreply.github.com>
2024-07-23 14:21:19 +0900

    Update UserController.java

--- src/main/java/ac/su/inclassspringsecurity/controller/UserController.java
+++ src/main/java/ac/su/inclassspringsecurity/controller/UserController.java
@@ -120,50 +120,50 @@ public class UserController {
 
     
 
-    @PostMapping("/signup")
-    public String createUser(
-        @Valid UserCreateForm userCreateForm,
-        BindingResult bindingResult
-    ) {
-        // 1. Form 데이터 검증
-        // (1-1) 입력값 바인딩 검사
-        if(bindingResult.hasErrors()) {
-            return "signup_form";
-        }
-        // (1-2) 입력값 내용 검사
-        if(!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
-            bindingResult.rejectValue(
-                "password2",
-                "passwordDoubleCheckError",
-                "패스워드 확인 값이 일치하지 않습니다."
-            );
-            return "signup_form";
-        }
+    // @PostMapping("/signup")
+    // public String createUser(
+    //     @Valid UserCreateForm userCreateForm,
+    //     BindingResult bindingResult
+    // ) {
+    //     // 1. Form 데이터 검증
+    //     // (1-1) 입력값 바인딩 검사
+    //     if(bindingResult.hasErrors()) {
+    //         return "signup_form";
+    //     }
+    //     // (1-2) 입력값 내용 검사
+    //     if(!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
+    //         bindingResult.rejectValue(
+    //             "password2",
+    //             "passwordDoubleCheckError",
+    //             "패스워드 확인 값이 일치하지 않습니다."
+    //         );
+    //         return "signup_form";
+    //     }
 
-        // 2. 백엔드 validation
-        try {
-            userService.create(
-                userCreateForm.getUsername(),
-                userCreateForm.getPassword1(),
-                userCreateForm.getEmail()
-            );
-        } catch (IllegalStateException e) {
-            bindingResult.reject(
-                "signupFailed",
-                "이미 등록된 사용자 입니다."
-            );
-            return "signup_form";
-        } catch (Exception e) {
-            bindingResult.reject(
-                "signupFailed",
-                e.getMessage()
-            );
-            return "signup_form";
-        }
+    //     // 2. 백엔드 validation
+    //     try {
+    //         userService.create(
+    //             userCreateForm.getUsername(),
+    //             userCreateForm.getPassword1(),
+    //             userCreateForm.getEmail()
+    //         );
+    //     } catch (IllegalStateException e) {
+    //         bindingResult.reject(
+    //             "signupFailed",
+    //             "이미 등록된 사용자 입니다."
+    //         );
+    //         return "signup_form";
+    //     } catch (Exception e) {
+    //         bindingResult.reject(
+    //             "signupFailed",
+    //             e.getMessage()
+    //         );
+    //         return "signup_form";
+    //     }
 
-        // 3. 회원 가입 성공
-        return "redirect:/";
-    }
+    //     // 3. 회원 가입 성공
+    //     return "redirect:/";
+    // }
 
     @GetMapping("/login")
     public String login() {
